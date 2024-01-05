a87cc4b6a1f34661b8939ab9bee8367d2396ff11
jirisemecky <jiri@semecky.cz>
2024-01-06 00:35:00 +0100

    Basic matching result screen.

--- lib/screens/matching_results_screen.dart
+++ lib/screens/matching_results_screen.dart
@@ -1,11 +1,43 @@
 import 'package:elections_match/models/data.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MatchingResultsScreen extends StatelessWidget {
-  const MatchingResultsScreen(Elections elections, {super.key});
+  final Elections elections;
+
+  const MatchingResultsScreen(this.elections, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('matching results screen');
+    return Scaffold(
+        appBar: AppBar(title: const Text('Matching candidates...')),
+        body: Column(
+            children: buildCandidateMatches() +
+                [
+                  ElevatedButton(
+                      onPressed: () => backToElection(),
+                      style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20)),
+                      child: const Text('Finish matching'))
+                ]));
   }
+
+  List<Widget> buildCandidateMatches() {
+    var partyMatches = <Widget>[];
+    for (final party in elections.parties) {
+      partyMatches.add(Row(
+        children: [
+          const Text(
+            '80%',
+            style: TextStyle(fontSize: 18),
+          ),
+          const Spacer(),
+          Text(party.name)
+        ],
+      ));
+    }
+    return partyMatches;
+  }
+
+  backToElection() {}
 }
